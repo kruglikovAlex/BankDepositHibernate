95a8322f889969699049cc00677c5c55710139fb
kruglikovAlex <kruglikov_alex@tut.by>
2021-11-29 22:45:16 +0300

    refactoring

--- VipiskaEGBD/app-dao/src/main/java/com/brest/egbd/dao/ApplicantDaoImp.java
+++ VipiskaEGBD/app-dao/src/main/java/com/brest/egbd/dao/ApplicantDaoImp.java
@@ -6,6 +6,7 @@ import com.brest.egbd.domain.Statement;
 import com.brest.egbd.util.HibernateUtil;
 
 import com.brest.egbd.util.HibernateUtil;
+import org.apache.logging.log4j.core.jackson.ListOfMapEntryDeserializer;
 import org.hibernate.Session;
 
 import org.apache.logging.log4j.Logger;
@@ -42,14 +43,14 @@ public class ApplicantDaoImp implements ApplicantDao{
     public List<Applicant> getApplicantsCriteria(){
         LOGGER.debug("getApplicantsCriteria()");
         applicants = new ArrayList<Applicant>();
-            //--- open session
+        //--- open session
         HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
-            //--- query
+        //--- query
         for(Object d: HibernateUtil.getSessionFactory().getCurrentSession()
                 .createCriteria(Applicant.class).list()){
             applicants.add((Applicant) d);
         }
-            //--- close session
+        //--- close session
         HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
 
         LOGGER.debug("applicants:{}", applicants);
@@ -58,17 +59,18 @@ public class ApplicantDaoImp implements ApplicantDao{
         //---- part
     @Override
     public List<Applicant> getApplicantsByDateOfBirthCriteria(Date dateOfBirth){
-        LOGGER.debug("getApplicantsByDateOfBirthCriteria({})", dateOfBirth);
+        LOGGER.debug("getApplicantsByDateOfBirthCriteria(dateOfBirth = {})", dateOfBirth);
         applicants = new ArrayList<Applicant>();
-            //--- open session
+        //--- open session
         HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
-            //--- query
+        //--- query
         for(Object d: HibernateUtil.getSessionFactory().getCurrentSession()
-                .createCriteria(Applicant.class).add(Restrictions.eq("applicantDateOfBirth", dateOfBirth))
+                .createCriteria(Applicant.class)
+                .add(Restrictions.eq("applicantDateOfBirth", dateOfBirth))
                 .list()){
             applicants.add((Applicant) d);
         }
-            //--- close session
+        //--- close session
         HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
 
         LOGGER.debug("applicants;{}", applicants);
@@ -77,13 +79,13 @@ public class ApplicantDaoImp implements ApplicantDao{
 
     @Override
     public List<Applicant> getApplicantByApplicantSurnameCriteria(String applicantSurname){
-        LOGGER.debug("getApplicantByApplicantSurnameCriteria({})", applicantSurname);
+        LOGGER.debug("getApplicantByApplicantSurnameCriteria(applicantSurname = {})", applicantSurname);
         //--- open session
         HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
         //--- query
         for(Object d: HibernateUtil.getSessionFactory().getCurrentSession()
                 .createCriteria(Applicant.class)
-                .add(Restrictions.eq("applicantSurname)", applicantSurname))
+                .add(Restrictions.eq("applicantSurname", applicantSurname))
                 .list()){
             applicants.add((Applicant) d);
         }
@@ -95,16 +97,60 @@ public class ApplicantDaoImp implements ApplicantDao{
     }
 
     public List<Applicant> getApplicantByApplicantNameCriteria(String applicantName){
+        LOGGER.debug("getApplicantByApplicantNameCriteria(applicantName = {})", applicantName);
+        //--- open session
+        HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+        //--- query
+        for(Object d: HibernateUtil.getSessionFactory().getCurrentSession()
+                .createCriteria(Applicant.class)
+                .add(Restrictions.eq("applicantName", applicantName))
+                .list()){
+            applicants.add((Applicant) d);
+        }
+        //--- close session
+        HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+
+        LOGGER.debug("applicants: {}", applicants);
         return applicants;
     }
 
     public List<Applicant> getApplicantByApplicantSurnameAndNameCriteria(String applicantSurname,
                                                                          String applicantName) {
+        LOGGER.debug("getApplicantByApplicantSurnameAndNameCriteria(applicantSurname = {},applicantName = {})",
+                applicantSurname,applicantName);
+        //--- open session
+        HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+        //--- query
+        for(Object d: HibernateUtil.getSessionFactory().getCurrentSession()
+                .createCriteria(Applicant.class)
+                .add(Restrictions.and(Restrictions.eq("applicantSurname",applicantSurname), Restrictions.eq("applicantName", applicantName)))
+                .list()) {
+            applicants.add((Applicant) d);
+        }
+        //or .add(Restrictions.eq("applicantSurname",applicantSurname)).add(Restrictions.eq("applicantName", applicantName));
+        //--- close session
+        HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+
+        LOGGER.debug("applicants: {}", applicants);
         return applicants;
     }
 
     public List<Applicant> getApplicantByApplicantFIOCriteria(String applicantSurname,
                                                               String applicantName, String applicantPatronymic) {
+        LOGGER.debug("getApplicantByApplicantFIOCriteria(applicantSurname = {},applicantName = {}, applicantPatronymic = {})",
+                applicantSurname, applicantName, applicantPatronymic);
+        //--- open session
+        HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+        //--- query
+        for(Object d: HibernateUtil.getSessionFactory().getCurrentSession()
+                .createCriteria(Applicant.class)
+                .add(Restrictions.and(Restrictions.eq("applicantSurname", applicantSurname),
+                        Restrictions.eq("applicantName", applicantName),
+                        Restrictions.eq("applicantPatronymic", applicantPatronymic))).list()){
+            applicants.add((Applicant) d);
+        }
+
+        LOGGER.debug("applicants: {}", applicants);
         return applicants;
     }
 
@@ -112,13 +158,13 @@ public class ApplicantDaoImp implements ApplicantDao{
     @Override
     public Applicant getApplicantByIdCriteria(Long applicantId){
         LOGGER.debug("getApplicantByIdCriteria({})", applicantId);
-            //--- open session
+        //--- open session
         HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
-            //--- query
+        //--- query
         applicant = (Applicant) HibernateUtil.getSessionFactory().getCurrentSession()
                 .createCriteria(Applicant.class)
                 .add(Restrictions.eq("applicantId", applicantId)).uniqueResult();
-            //--- close session
+        //--- close session
         HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
 
         LOGGER.debug("applicant:{}", applicant);
@@ -126,19 +172,74 @@ public class ApplicantDaoImp implements ApplicantDao{
     }
 
     public Applicant getApplicantByIdentificationNumberCriteria(String applicantIdentificationNumber) {
+        LOGGER.debug("getApplicantByIdentificationNumberCriteria({})", applicantIdentificationNumber);
+        //--- open session
+        HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+        //--- query
+        applicant = (Applicant) HibernateUtil.getSessionFactory().getCurrentSession()
+                .createCriteria(Applicant.class)
+                .add(Restrictions.eq("applicantIdentificationNumber", applicantIdentificationNumber))
+                .uniqueResult();
+        //--- close session
+        HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+
+        LOGGER.debug("applicant = {}", applicant);
         return applicant;
     }
 
         //---- create
     public void addApplicant(Applicant applicant){
+        LOGGER.debug("addApplicant({})", applicant);
         assertNotNull(applicant);
+        try{
+            //--- open session
+            HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+            //--- save object
+            HibernateUtil.getSessionFactory()
+                    .getCurrentSession().save(applicant);
+            //--- close session
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+        }catch (Exception e){
+            LOGGER.error("error - addApplicant({}) - {}", applicant, e.toString());
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
+            throw new IllegalArgumentException("error - addApplicant() " + e.toString());
+        }
     }
         //---- update
     public void updateApplicant(Applicant applicant){
+        LOGGER.debug("updateApplicant({})", applicant);
         assertNotNull(applicant);
+        try{
+            //--- open session
+            HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+            //--- update
+            HibernateUtil.getSessionFactory()
+                    .getCurrentSession().update(applicant);
+            //--- close session
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+        }catch (Exception e){
+            LOGGER.error("error - updateApplicant({}) - {}", applicant, e.toString());
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
+            throw new IllegalArgumentException("error - updateApplicant() " + e.toString());
+        }
     }
         //---- delete
     public void deleteApplicant(Long applicantId){
+        LOGGER.debug("deleteApplicant({})",applicantId);
         assertNotNull(applicantId);
+        try{
+            //--- open session
+            HibernateUtil.getSessionFactory().getCurrentSession().beginTransaction();
+            //--- query
+            Applicant applicant = (Applicant) HibernateUtil.getSessionFactory().getCurrentSession()
+                    .createCriteria(Applicant.class)
+                    .add(Restrictions.eq("applicantId", applicantId)).uniqueResult();
+            //--- delete
+            HibernateUtil.getSessionFactory().getCurrentSession().delete(applicant);
+        }catch (Exception e){
+            LOGGER.error("error - deleteApplicant({}) - {}", applicant, e.toString());
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
+            throw new IllegalArgumentException("error - deleteApplicant() " + e.toString());
+        }
     }
 }
